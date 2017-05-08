bbbd31a5efbd15b54b1e63534714e924d71750f3
anindya-duti <duti.webhawksit@gmail.com>
2017-05-08 17:09:53 +0600

    remove volley related resources from the project

--- app/src/main/java/anindya/sample/smackchat/utils/Const.java
+++
@@ -1,8 +0,0 @@
-package anindya.sample.smackchat.utils;
-
-public class Const {
-	public static final String URL_JSON_OBJECT = "http://123.200.14.11:9090/plugins/restapi/v1/users?username=anindya&password=anindya2012";
-	public static final String URL_JSON_ARRAY = "http://api.androidhive.info/volley/person_array.json";
-	public static final String URL_STRING_REQ = "http://api.androidhive.info/volley/string_response.html";
-	public static final String URL_IMAGE = "http://api.androidhive.info/volley/volley-image.jpg";
-}
