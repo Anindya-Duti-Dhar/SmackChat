5666ad5ef4b735abd825b5dec3acec845561df93
Duti <anindyadutidhar@yahoo.com>
2017-09-20 11:03:22 +0600

    server ip note

--- app/src/main/java/anindya/sample/smackchat/utils/Const.java
+++ app/src/main/java/anindya/sample/smackchat/utils/Const.java
@@ -3,7 +3,7 @@ package anindya.sample.smackchat.utils;
 public class Const {
 
 	//openFire server communication data
-	public static final String CHAT_SERVER_ADDRESS = "123.200.14.11";   // "153.126.152.115";
+	public static final String CHAT_SERVER_ADDRESS = "123.200.14.11";   // "153.126.152.115"; // you could replace with your local pc ip if mobile device is in same network
 	public static final int CHAT_SERVER_PORT = 5222;
 	public static final String CHAT_SERVER_SERVICE_NAME = "webhawksit";  // "153.126.152.115";
 	public static final String CHAT_SERVER_RESOURCE_NAME = "Android";
