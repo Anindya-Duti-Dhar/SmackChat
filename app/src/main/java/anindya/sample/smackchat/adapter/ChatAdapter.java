5de0c05df3f5e15385afc4bed2f66a57a4ab8071
Duti <anindyadutidhar@yahoo.com>
2017-09-20 22:15:45 +0600

    chat layout customization

--- app/src/main/java/anindya/sample/smackchat/adapter/ChatAdapter.java
+++ app/src/main/java/anindya/sample/smackchat/adapter/ChatAdapter.java
@@ -5,25 +5,31 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 import anindya.sample.smackchat.R;
 import anindya.sample.smackchat.model.ChatItem;
+import anindya.sample.smackchat.utils.PrefManager;
 
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView chat_user_without_image, chat_username, chat_user_chat;
+        public TextView chat_user_without_image, chat_username, chat_user_chat, my_user_chat;
+        public LinearLayout friend_chat, my_chat;
 
         public ViewHolder(View itemView) {
             super(itemView);
             chat_username = (TextView) itemView.findViewById(R.id.chat_username);
             chat_user_chat = (TextView) itemView.findViewById(R.id.chat_user_chat);
             chat_user_without_image = (TextView) itemView.findViewById(R.id.chat_user_without_image);
+            my_user_chat = (TextView) itemView.findViewById(R.id.my_user_chat);
+            friend_chat = (LinearLayout) itemView.findViewById(R.id.friend_chat);
+            my_chat = (LinearLayout) itemView.findViewById(R.id.my_chat);
         }
     }
 
@@ -53,27 +59,44 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
         // Get the data model based on position
         final ChatItem data = _data.get(position);
+        LinearLayout FriendChat = viewHolder.friend_chat;
+        LinearLayout MyChat = viewHolder.my_chat;
+        if(data.getChatUserName().equals(PrefManager.getUserName(mContext))){
+            FriendChat.setVisibility(View.GONE);
+            MyChat.setVisibility(View.VISIBLE);
 
-        TextView ChatUserNOImage = viewHolder.chat_user_without_image;
-        try {
-            ChatUserNOImage.setText(String.valueOf(data.getChatUserName().toString().charAt(0)));
-        } catch (Exception e) {
-            e.printStackTrace();
+            TextView MyUserChat = viewHolder.my_user_chat;
+            try {
+                MyUserChat.setText(data.getChatText());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+        else {
+            FriendChat.setVisibility(View.VISIBLE);
+            MyChat.setVisibility(View.GONE);
 
-        TextView ChatUsername = viewHolder.chat_username;
-        try {
-            String desiredUsername = data.getChatUserName();
-            ChatUsername.setText(toInitCap(desiredUsername));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            TextView ChatUserNOImage = viewHolder.chat_user_without_image;
+            try {
+                ChatUserNOImage.setText(String.valueOf(data.getChatUserName().toString().charAt(0)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-        TextView ChatUserChat = viewHolder.chat_user_chat;
-        try {
-            ChatUserChat.setText(data.getChatText());
-        } catch (Exception e) {
-            e.printStackTrace();
+            TextView ChatUsername = viewHolder.chat_username;
+            try {
+                String desiredUsername = data.getChatUserName();
+                ChatUsername.setText(toInitCap(desiredUsername));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            TextView ChatUserChat = viewHolder.chat_user_chat;
+            try {
+                ChatUserChat.setText(data.getChatText());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
