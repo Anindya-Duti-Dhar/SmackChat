44b6703fb3a9e5e343842c1b2373f79eb6f0d463
Duti <anindyadutidhar@yahoo.com>
2018-08-29 21:07:49 +0600

    update gradle

--- app/src/main/java/anindya/sample/smackchat/utils/Const.java
+++ app/src/main/java/anindya/sample/smackchat/utils/Const.java
@@ -3,9 +3,9 @@ package anindya.sample.smackchat.utils;
 public class Const {
 
 	//openFire server communication data
-	public static final String CHAT_SERVER_ADDRESS = "123.200.14.11";   // "153.126.152.115"; // you could replace with your local pc ip if mobile device is in same network
+	public static final String CHAT_SERVER_ADDRESS = "192.168.0.100"; //"123.200.14.11";   // "153.126.152.115"; // you could replace with your local pc ip if mobile device is in same network
 	public static final int CHAT_SERVER_PORT = 5222;
-	public static final String CHAT_SERVER_SERVICE_NAME = "webhawksit";  // "153.126.152.115";
+	public static final String CHAT_SERVER_SERVICE_NAME = "desktop-r1pbkha";//"webhawksit";  // "153.126.152.115";
 	public static final String CHAT_SERVER_RESOURCE_NAME = "Android";
 	public static final String CHAT_ROOM_SERVICE_NAME = "conference.";
 	public static final String ALTERNATE_CHAT_ROOM_REFERENCE = "demo";
